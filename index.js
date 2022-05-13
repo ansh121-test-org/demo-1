2fffc1e87ae6e5d58cfa58ec379d094836ade88c
Balaga Gayatri <balaga-gayatri@github.com>
2022-05-13 17:23:32 +0530

    changes

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-const github = require('@actions/github');
-const core = require('@actions/core');
+import { getOctokit, context as _context } from '@actions/github';
+import { getInput, setOutput, setFailed } from '@actions/core';
 
 async function run() {
-  console.log("hello..............");
-  // try {
-  //   const token = core.getInput('token');
-  //   const octokit = github.getOctokit(token);
-  //   const context = github.context;
+  // console.log("hello..............");
+  try {
+    const token = getInput('token');
+    const octokit = getOctokit(token);
+    const context = _context;
 
-  //   const issuesAndPulls = await octokit.paginate(octokit.rest.issues.listForRepo, {
-  //     owner: context.repo.owner,
-  //     repo: context.repo.repo,
-  //     state: 'all',
-  //   });
+    const issuesAndPulls = await octokit.paginate(octokit.rest.issues.listForRepo, {
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      state: 'all',
+    });
 
-  //   const issue_stats = { open: 0, closed: 0 }
+    const issue_stats = { open: 0, closed: 0 }
 
-  //   const pull_stats = { open: 0, closed: 0, merged: 0 }
+    const pull_stats = { open: 0, closed: 0, merged: 0 }
 
-  //   for(const item of issuesAndPulls){
-  //     if('pull_request' in item) {
-  //       if(item.state == 'open') pull_stats.open ++;
-  //       else{
-  //         if(item.merged_at == null) pull_stats.closed ++;
-  //         else pull_stats.merged ++;
-  //       }
-  //     }
-  //     else {
-  //       if(item.state == 'open') issue_stats.open ++;
-  //       else issue_stats.closed ++;
-  //     }
-  //   }
+    for(const item of issuesAndPulls){
+      if('pull_request' in item) {
+        if(item.state == 'open') pull_stats.open ++;
+        else{
+          if(item.merged_at == null) pull_stats.closed ++;
+          else pull_stats.merged ++;
+        }
+      }
+      else {
+        if(item.state == 'open') issue_stats.open ++;
+        else issue_stats.closed ++;
+      }
+    }
 
-  //   core.setOutput('pulls', pull_stats);
-  //   core.setOutput('issues', issue_stats);
-  // } catch (error) {
-  //   core.setFailed(error.message);
-  // }
+    setOutput('pulls', pull_stats);
+    setOutput('issues', issue_stats);
+  } catch (error) {
+    setFailed(error.message);
+  }
 }
 
 run();
